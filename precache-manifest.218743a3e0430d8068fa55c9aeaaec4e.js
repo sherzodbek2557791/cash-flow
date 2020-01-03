66ec94770f187ea48cf4fa2f70db37b7d3f68931
Sherzodbek Abdullaev <leonbek2557791@gmail.com>
2020-01-03 18:40:00 +0500

    Add files via upload

---
+++ precache-manifest.218743a3e0430d8068fa55c9aeaaec4e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cce4db6cb402c58356b",
+    "url": "/css/app.05b0fd4b.css"
+  },
+  {
+    "revision": "a0279b576599ef3d0af04918903e265b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cce4db6cb402c58356b",
+    "url": "/js/app.71d38bf6.js"
+  },
+  {
+    "revision": "59f8f66ee0e25ace863c",
+    "url": "/js/chunk-2d22d746.bcdcf035.js"
+  },
+  {
+    "revision": "d4609721965cb3a206ce",
+    "url": "/js/chunk-7b963897.a8faa652.js"
+  },
+  {
+    "revision": "46fc4d993af32859586a",
+    "url": "/js/chunk-vendors.6a1c28cd.js"
+  },
+  {
+    "revision": "829d8803a68530537239a2bb145b3480",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
